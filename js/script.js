72fdeb2b636f9c1a8492a24344ce04c35404c6b3
khan-salman1 <salmankhan03102496905@gmail.com>
2025-03-28 11:59:53 +0000

    deleted

--- js/script.js
+++
@@ -1,6 +0,0 @@
-function savePaintingDetails(paintingTitle, paintingDetails) {
-    var doc = new jsPDF();
-    doc.text(`Painting Title: ${paintingTitle}`, 10, 10);
-    doc.text(`Details: ${paintingDetails}`, 10, 20);
-    doc.save(`${paintingTitle}.pdf`);
-}
